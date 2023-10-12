9acd485899bb4dd02cdf0311bdad1ad2aea32f26
Marek Martin <marekwade1519@gmail.com>
2023-10-12 19:42:42 +0000

    made new dir for easy 5

---
+++ small_problems/acsii_string_value.rb
@@ -0,0 +1,30 @@
+=begin 
+problem
+
+Write a method that determines and returns the ASCII string value of a string that is passed in as an argument.
+The ASCII string value is the sum of the ASCII values of every character in the string. 
+(You may use String#ord to determine the ASCII value of a character.)
+
+ex:
+ascii_value('Four score') == 984
+ascii_value('Launch School') == 1251
+ascii_value('a') == 97
+ascii_value('') == 0
+
+P 
+Input: 
+Output:
+Rules:
+  -Explicit Requirements
+  
+  -Implicit Requirements
+  
+D
+- What data structures could we use to solve this problem?
+- What does our data look like when we get it? (input)
+- What does our data look like when we return it? (output?)
+- What does our data need to look like in the intermediate steps?
+
+A 
+
+=end 
